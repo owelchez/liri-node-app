2d76e18c131e3327e1a05baf66e01fb2e8f2507a
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-07-15 19:49:40 -0400

    Spoty

--- liri.js
+++ liri.js
@@ -1,24 +1,4 @@
-/*spotify-this-song
-
-movie-this
-
-do-what-it-says
-
-node liri.js my-tweets
-
-This will show your last 20 tweets and when they were created at in your terminal/bash window.
-node liri.js spotify-this-song '<song name here>'
-
-This will show the following information about the song in your terminal/bash window
-
-Artist(s)
-The song's name
-A preview link of the song from spotify
-The album that the song is from
-if no song is provided then your program will default to
-
-"The Sign" by Ace of Base
-node liri.js movie-this '<movie name here>'
+/*node liri.js movie-this '<movie name here>'
 
 This will output the following information to your terminal/bash window:
 
@@ -48,23 +28,27 @@ Make sure you append each command you run to the log.txt file.
 
 Do not overwrite your file each time you run a command.*/
 
-//my-tweets
+/*******************NPM Packages **********************/
+var twitter = require('twitter');
+var spotify = require('spotify');
 
 var action = process.argv[2];
+var song = process.argv[3];
+var song1 = process.argv[4];
+var song2 = process.argv[5];
+var song3 = process.argv[6];
+var song4 = process.argv[7];
 
+						/*This is for Twitter*/
 if(action === 'my-tweets'){
-
-var Twitter = require('twitter');
- 
-var client = new Twitter({
+var client = new twitter({
   consumer_key: 'HmjS9pB8vwmb4IGurZHz6VgCD',
   consumer_secret: 'LurEfY85cVG8ooKQmbST13N2snLePAouX0w2XSwuSmcRBHJV0A',
   access_token_key: '2881919943-E1vKtRkprCb90XNTsIJKpk2wSVBAsKHxIgGhKs0',
   access_token_secret: 'JEG0ZdjqCjSroRepVsdj1gn8xsKmqUckUKSG2Nc21e271'
 });
  
-var params =	{screen_name: 'zerox504',
-				 text: 'text'};
+var params =	{screen_name: 'zerox504'};
 client.get('statuses/user_timeline', params, function(error, tweets, response){
   if (!error) {
   	var tweetsLength = tweets.length;
@@ -76,11 +60,38 @@ client.get('statuses/user_timeline', params, function(error, tweets, response){
   	}
   }
 });
+						/*This is for Spotify*/
+} else if (action === 'spotify-this-song'){	
+	spotify.search({ type: 'track', query: song + song1 + song2 + song3 + song4 }, function(err, data) {
+    if (err) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+ 	var music = JSON.stringify(data);
+ 	console.log(music);
+
+
+});
+
+
+
+
+
+console.log("You are the shit!");
 }
 
 
 
 
+/*node liri.js spotify-this-song '<song name here>
+This will show the following information about the song in your terminal/bash window
 
+Artist(s)
+The song's name
+A preview link of the song from spotify
+The album that the song is from
+if no song is provided then your program will default to
 
+"The Sign" by Ace of Base*/
 
