b79692a1d53c64da95d01230d686d999bdb3a7a5
Orvin Welchez <owelchez@gmail.com>
2016-07-16 09:47:19 -0400

    Spoty

--- liri.js
+++ liri.js
@@ -1,33 +1,3 @@
-/*node liri.js movie-this '<movie name here>'
-
-This will output the following information to your terminal/bash window:
-
-Title of the movie.
-Year the movie came out.
-IMDB Rating of the movie.
-Country where the movie was produced.
-Language of the movie.
-Plot of the movie.
-Actors in the movie.
-Rotten Tomatoes Rating.
-Rotton Tomatoes URL.
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-It's on Netflix!
-node liri.js do-what-it-says
-
-Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-Feel free to change the text in that document to test out the feature for other commands.
-BONUS
-
-In addition to logging the data to your terminal/bash window, output the data to a .txt file called log.txt.
-
-Make sure you append each command you run to the log.txt file.
-
-Do not overwrite your file each time you run a command.*/
-
 /*******************NPM Packages **********************/
 var twitter = require('twitter');
 var spotify = require('spotify');
@@ -68,30 +38,8 @@ client.get('statuses/user_timeline', params, function(error, tweets, response){
         return;
     }
 
- 	var music = JSON.stringify(data);
- 	console.log(music);
+ 	console.log(data);
 
 
 });
-
-
-
-
-
-console.log("You are the shit!");
 }
-
-
-
-
-/*node liri.js spotify-this-song '<song name here>
-This will show the following information about the song in your terminal/bash window
-
-Artist(s)
-The song's name
-A preview link of the song from spotify
-The album that the song is from
-if no song is provided then your program will default to
-
-"The Sign" by Ace of Base*/
-
---
+++ tarea.js
@@ -0,0 +1,15 @@
+var myObject = {
+myName: name,
+myBirthdayMonth: month,
+/*myAlertFunc = function(string){
+alert('string');
+}
+myAdditionFunc = function(a,b){
+return a+b;
+}*/
+}
+
+
+
+
+console.log(myObject.myName);
